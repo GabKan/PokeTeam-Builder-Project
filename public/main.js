3d76055e2fd34d59ccedf00015df1a5514a0e072
klrmsngh <kyleramsingh17@gmail.com>
2025-04-10 23:03:20 -0400

    added display list etc

--- public/main.js
+++ public/main.js
@@ -1,8 +1,22 @@
 let pokemon_url = [];
+let pokemon=[];
 
 function display_pokemon() {
     for(let pokemon of pokemon_url)
         console.log(pokemon);
+}
+
+function display_list(list_num){
+    let list=document.querySelector(`#pokeList${list_num}`);
+    let html='';
+    for(let i=0;i<pokemon.length;i++){
+       html += `<li><a href="#" onclick="display_pokemon_info(${i})">${pokemon[i].name}</a></li>`
+    }
+    list.innerHTML=html;
+}
+
+function display_pokemon_info(id){
+
 }
 
 async function get_all_pokemon() {
@@ -11,10 +25,11 @@ async function get_all_pokemon() {
     const data = await response.json();
 
     for(let result of data.results) {
-        pokemon_url.push(result.url);
+       pokemon_url.push(result.url);
+        pokemon.push(result);
     }
 
-    // display_pokemon();
+    //display_pokemon();
 }
 
 async function get_pokemon(id) {
@@ -53,6 +68,27 @@ function submit(event) {
 async function main() {
     await get_all_pokemon();
     document.forms['my-form'].addEventListener('submit', submit);
+}
+
+function filterPokemon(list_num){
+    let input=document.getElementById("pokeSearch1");
+    let capInput=input.value.toUpperCase();
+    let ul=document.getElementById(`pokeList${list_num}`);
+    let li=ul.getElementsByTagName("li");
+
+    if(capInput.length >0)
+        ul.style.display="block";
+    else
+        ul.style.display="none";
+
+    for(let i of li){
+        let pokemon_name=i.textContent.toUpperCase();
+        if(pokemon_name.indexOf(capInput)>-1)
+            i.style.display="";
+        else
+            i.style.display="none";
+    }
+        
 }
 
 main();
