d692e94f721f38821d7e54262f34680a32f236d7
Keshavm2 <keshavmaharaj45@gmail.com>
2025-04-13 16:14:15 -0400

    updated firebase

--- public/firebaseConfig.js
+++ public/firebaseConfig.js
@@ -1,10 +1,10 @@
 const firebaseConfig = {
-    apiKey: "***************************************",
-    authDomain: "poketeam-builder-60adb.firebaseapp.com",
-    projectId: "poketeam-builder-60adb",
-    storageBucket: "poketeam-builder-60adb.firebasestorage.app",
-    messagingSenderId: "704516413938",
-    appId: "1:704516413938:web:ee7dcbdb6233631972b36b"
+    apiKey: "AIzaSyDPQB8oinINF30fcChCu9Mid39O88BxzYU",
+    authDomain: "pokemon-team-builder-a9db3.firebaseapp.com",
+    projectId: "pokemon-team-builder-a9db3",
+    storageBucket: "pokemon-team-builder-a9db3.firebasestorage.app",
+    messagingSenderId: "129209427568",
+    appId: "1:129209427568:web:a3940c71abe464f0ba36b5"
 };
 
-export default firebaseConfig
+export default firebaseConfig;
